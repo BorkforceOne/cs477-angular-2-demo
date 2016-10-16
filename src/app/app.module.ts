01594f43803adaeed04c52f6bde5bb62c6d7ccb2
Brandon Garling <bjg96@nau.edu>
2016-10-16 09:56:43 -0700

    Added FlagFriends sample files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
+import { AlertModule } from 'ng2-bootstrap';
 
+import { NavbarComponent } from './navbar/navbar.component';
 import { AppComponent } from './app.component';
+import { ResultComponent } from './result/result.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ResultComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'search', component: ResultComponent },
+      { path: '', component: DashboardComponent},
+    ]),
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
